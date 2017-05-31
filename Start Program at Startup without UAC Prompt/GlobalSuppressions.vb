1650c97ac4a3a6c3850ba5a592d3b693fc36a62a
Tom Parkison <trparky@toms-world.org>
2017-05-31 13:55:45 -0400

    Added code suppressions.

---
+++ Start Program at Startup without UAC Prompt/GlobalSuppressions.vb
@@ -0,0 +1,9 @@
+﻿
+' This file is used by Code Analysis to maintain SuppressMessage 
+' attributes that are applied to this project.
+' Project-level suppressions either have no target or are given 
+' a specific target and scoped to a namespace, type, member, etc.
+
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification:="<Pending>", Scope:="member", Target:="~M:Start_Program_at_Startup_without_UAC_Prompt.httpHelper.addFileUpload(System.String,System.String,System.String,System.String,System.Boolean)")>
+<Assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0017:Simplify object initialization", Justification:="<Pending>", Scope:="member", Target:="~M:Start_Program_at_Startup_without_UAC_Prompt.httpHelper.downloadStatusUpdateInvoker")>
