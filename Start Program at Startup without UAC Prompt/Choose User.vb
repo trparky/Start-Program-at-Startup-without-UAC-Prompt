2507348bef1e0de0ef183a3d7e2f6a24e44af522
Tom Parkison <trparky@toms-world.org>
2019-10-24 14:49:15 -0400

    Added Exception Handling to the Choose User window.

--- Start Program at Startup without UAC Prompt/Choose User.vb
+++ Start Program at Startup without UAC Prompt/Choose User.vb
@@ -2,13 +2,18 @@
     Public Property strSelectedUser As String
 
     Private Sub Choose_User_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Using usersSearcher As New Management.ManagementObjectSearcher("SELECT LocalAccount, Disabled, Lockout, SIDType, Name, Caption FROM Win32_UserAccount")
-            For Each user As Management.ManagementObject In usersSearcher.Get()
-                If CBool(user("LocalAccount")) AndAlso Not CBool(user("Disabled")) AndAlso Not CBool(user("Lockout")) AndAlso Integer.Parse(user("SIDType").ToString()) = 1 AndAlso Not user("Name").ToString.Equals("HomeGroupUser$", StringComparison.OrdinalIgnoreCase) Then
-                    listUsers.Items.Add(user("Caption").ToString)
-                End If
-            Next
-        End Using
+        Try
+            Using usersSearcher As New Management.ManagementObjectSearcher("SELECT LocalAccount, Disabled, Lockout, SIDType, Name, Caption FROM Win32_UserAccount")
+                For Each user As Management.ManagementObject In usersSearcher.Get()
+                    If CBool(user("LocalAccount")) AndAlso Not CBool(user("Disabled")) AndAlso Not CBool(user("Lockout")) AndAlso Integer.Parse(user("SIDType").ToString()) = 1 AndAlso Not user("Name").ToString.Equals("HomeGroupUser$", StringComparison.OrdinalIgnoreCase) Then
+                        listUsers.Items.Add(user("Caption").ToString)
+                    End If
+                Next
+            End Using
+        Catch ex As Exception
+            MsgBox("Unable to enumerate users on current system.", MsgBoxStyle.Critical, "Choose User...")
+            Me.Close()
+        End Try
     End Sub
 
     Private Sub listUsers_SelectedIndexChanged(sender As Object, e As EventArgs) Handles listUsers.SelectedIndexChanged
