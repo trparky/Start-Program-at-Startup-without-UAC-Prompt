24b7620743fe568306f647f6b18510bfae9a8833
Tom Parkison <trparky@toms-world.org>
2021-03-23 13:13:03 -0400

    Removed some constants.

--- Start Program at Startup without UAC Prompt/Module.vb
+++ Start Program at Startup without UAC Prompt/Module.vb
@@ -3,10 +3,6 @@ Imports System.Security.Principal
 Imports System.Text.RegularExpressions
 
 Module Globals
-    Public Const strOK As String = "OK"
-    Public Const strNo As String = "No"
-    Public Const strYes As String = "Yes"
-
     ' PHP like addSlashes and stripSlashes. Call using String.addSlashes() and String.stripSlashes().
     <Extension()>
     Public Function addSlashes(unsafeString As String) As String
