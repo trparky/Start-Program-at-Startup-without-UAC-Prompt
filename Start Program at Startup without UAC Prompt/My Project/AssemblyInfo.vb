e9a7a8be79e71ccce09f0754bf4687efc09ebee8
Thomas Parkison <trparky@toms-world.org>
2023-08-10 14:55:26 -0400

    Updated copyright in AssemblyInfo.vb.

--- Start Program at Startup without UAC Prompt/My Project/AssemblyInfo.vb
+++ Start Program at Startup without UAC Prompt/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("Start Program at Startup without UAC Prompt")> 
-<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyCopyright("Copyright Thomas Parkison © 2023-2024")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
