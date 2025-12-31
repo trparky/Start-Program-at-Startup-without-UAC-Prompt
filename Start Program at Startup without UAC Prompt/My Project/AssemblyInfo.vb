e2137addc70cf7fcf210879e93626bb6234f9e5f
Thomas Parkison <trparky@toms-world.org>
2025-12-31 15:04:50 -0500

    Updated the version.

--- Start Program at Startup without UAC Prompt/My Project/AssemblyInfo.vb
+++ Start Program at Startup without UAC Prompt/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("2.3.17.16")>
+<Assembly: AssemblyFileVersion("2.4.1.17")>
