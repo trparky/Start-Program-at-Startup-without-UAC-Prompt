c514bacc21f75774f73c95bb6d1357ec31ac8b6f
Thomas Parkison <trparky@toms-world.org>
2026-09-03 16:30:05 -0400

    Removed two unnecessary imports lines.

--- Start Program at Startup without UAC Prompt/Program Modules/Check for Update Stuff.vb
+++ Start Program at Startup without UAC Prompt/Program Modules/Check for Update Stuff.vb
@@ -1,8 +1,6 @@
 ﻿Imports System.IO
 Imports System.Text.RegularExpressions
 Imports System.Xml
-Imports System.Security.AccessControl
-Imports System.Security.Principal
 
 Namespace checkForUpdates
     Module checkForUpdatesModule
